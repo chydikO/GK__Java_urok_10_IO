29d1fa0f20938f8cb69080c9a5e8647e537ee20f
x220_Chudnovskiy0 <oleg.chudnovskiy@gmail.com>
2023-07-20 19:05:24 +0300

    add App011 -> InputStreamReader & OutputStreamWriter

--- src/main/java/org/example/App010.java
+++ src/main/java/org/example/App010.java
@@ -6,17 +6,26 @@ import java.util.logging.Logger;
 
 public class App010 {
     public static void main(String[] args) {
-        try (FileReader fileReader = new FileReader("lines.txt");
-             FileWriter fileWriter = new FileWriter("lines_out.txt", true);
+        try (InputStreamReader fileReader = new FileReader("lines.txt");
+             OutputStreamWriter fileWriter = new FileWriter("lines_out.txt", true);
              BufferedReader bufferedReader = new BufferedReader(fileReader);) {
-            String line;
+            String line = null;
             int lineCounter = 0;
-            while ((line = bufferedReader.readLine()) != null) {
-                if ((lineCounter++) % 2 == 0) {
-                    System.out.println(line);
-                    fileWriter.write(line+System.getProperty("line.separator"));
-                }
+//            while ((line = bufferedReader.readLine()) != null) {
+//                if ((lineCounter++) % 2 == 0) {
+//                    System.out.println(line);
+//                    fileWriter.write(line+System.getProperty("line.separator"));
+//                }
+            char[] buffer = new char[(int)new File("lines.txt").length()];
+            bufferedReader.read(buffer);
+            //line.getChars(0, line.length(), buffer, 0);
+            for (int i = 0; i < buffer.length; i++) {
+                if (buffer[i] == 'e')
+                    fileWriter.write('E');
+                else
+                    fileWriter.write(buffer[i]);
             }
+            fileWriter.write(System.getProperty("line.separator"));
         } catch (Exception exception) {
             Logger.getLogger(App010.class.getName()).log(Level.SEVERE, null, exception);
         }
